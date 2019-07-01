41e4eeef04c3a35251bff6297a4020d3e5ae81f6
asekdev <andy.sek94@gmail.com>
2019-07-01 13:34:31 +1000

    added figlet, started user authentication prompts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import Singleton.BotSingleton;
+import Utility.UserDetails;
+import com.github.lalyos.jfiglet.FigletFont;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,28 +13,52 @@ public class Main {
     public static void main(String[] args){
 
         //setup the singleton
+
+
         BotSingleton singleton = BotSingleton.getInstance();
         singleton.setDriver(setupChromeDriver());
+        String headerText = "";
 
         boolean programRunning = true;
         Scanner scan = new Scanner(System.in);
 
-        //start the initial
+        try {
+             headerText = FigletFont.convertOneLine("autogram");
 
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        //start the initial
+        System.out.println(headerText);
         System.out.println("Welcome to Instagram Automation Bot!");
         System.out.println("To get started, specify your username and password for Instagram.\n");
 
+        UserDetails user = getUserCredentials();
 
-
-
+        System.out.println("username of object: " + user.getUsername());
+        System.out.println("password of object: " + user.getPassword());
 
 
     }
 
     public static WebDriver setupChromeDriver() {
         System.setProperty("webdriver.chrome.driver", "/Users/723352/Downloads/chromedriver");
-        WebDriver driver = new ChromeDriver(); //re-add options as parameter
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options); //re-add options as parameter
         return driver;
+    }
+
+    public static UserDetails getUserCredentials() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Username: ");
+        String username = scan.next();
+
+        System.out.print("Password: ");
+        String password = scan.next();
+
+        return new UserDetails(username, password);
     }
 
 }
