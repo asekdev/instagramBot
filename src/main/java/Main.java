f5cc544a128e6b489c1764670010762733eb2181
asekdev <andy.sek94@gmail.com>
2019-07-01 12:11:02 +1000

    started main engine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
+import Singleton.BotSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
 
         //setup the singleton
+        BotSingleton singleton = BotSingleton.getInstance();
+        singleton.setDriver(setupChromeDriver());
 
         boolean programRunning = true;
+        Scanner scan = new Scanner(System.in);
 
         //start the initial
+
+        System.out.println("Welcome to Instagram Automation Bot!");
+        System.out.println("To get started, specify your username and password for Instagram.\n");
+
+
 
 
 
